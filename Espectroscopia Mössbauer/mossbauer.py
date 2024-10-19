f81ef6b9afe49752965341e2acb65e9049366c7a
Victor Emanuel <vg255408@gmail.com>
2024-10-19 09:46:45 -0300

    finalizado

--- Espectroscopia Mössbauer/mossbauer.py
+++ Espectroscopia Mössbauer/mossbauer.py
@@ -5,6 +5,8 @@ from scipy.signal import find_peaks
 import numpy as np
 #tratamento_Isomerico de dados
 def tratamento_Isomerico(file_name,cor,relacoao_ao_alfa=True):
+    # calcula o desvio isometrico em relação ao ferro-alfa
+    
     #dados a serem tratados
     dados=pd.read_csv(file_name, sep='\t',usecols=[0,2] ,names=["Velocidade da fonte (mm/s)","Contagem de fontos"])
     x = dados["Velocidade da fonte (mm/s)"]
@@ -13,7 +15,8 @@ def tratamento_Isomerico(file_name,cor,relacoao_ao_alfa=True):
     #plot do grafico
     plt.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
     if relacoao_ao_alfa==True:
-        plt.axvline(x=0.84, color='black', linestyle='-')#plotando uma linha vertical em Fe_x=0 para ver justamente o desvio a Fe_alfa cujo desvio isometrico é 0
+        #plotando uma linha vertical em Fe_x=0 para ver justamente o desvio a Fe_alfa cujo desvio isometrico é 0
+        plt.axvline(x=0.84, color='black', linestyle='-')
     grafico=plt.scatter(x,y, label=legenda_graficos,s=2, color=cor)
     cursor = mplcursors.cursor(grafico)
     plt.xlabel('Velocidade da fonte (mm/s)')
@@ -31,6 +34,7 @@ tratamento_Isomerico('Espectroscopia Mössbauer\Dados\Mossbauer_data\Hematita.tx
 
 #intereção quadrupolar
 def interacao_quadruplar(file_name, cor, altura):
+    #calcula os pontos de interação quadrupolar
     # Ler dados do arquivo
     dados = pd.read_csv(file_name, sep='\t', usecols=[0, 2], names=["Velocidade da fonte (mm/s)", "Contagem de fontos"])
     x = dados["Velocidade da fonte (mm/s)"]
@@ -65,7 +69,6 @@ def interacao_quadruplar(file_name, cor, altura):
     plt.plot(x_lim.iloc[picos], y_lim.iloc[picos], "ro")  # Usar índices relativos ao intervalo filtrado
     posições_picos=x_lim.iloc[picos].values
     
-    diferenca=x_lim.iloc[picos].diff()
     # Configurar cursor interativo
     plt.scatter(x_lim.iloc[picos], y_lim.iloc[picos],label=posições_picos, s=2)
     mplcursors.cursor(grafico)
@@ -79,12 +82,13 @@ def interacao_quadruplar(file_name, cor, altura):
     # Salvar e mostrar o gráfico
     plt.savefig("Espectroscopia Mössbauer/graficos_Quadrupolar/" + legenda_graficos + '.png')
     plt.show()
-# interacao_quadruplar('Espectroscopia Mössbauer\Dados\Mossbauer_data\Fe-alpha.txt')
-# interacao_quadruplar(r'Espectroscopia Mössbauer\Dados\Mossbauer_data\aco_inox.txt')
+interacao_quadruplar('Espectroscopia Mössbauer\Dados\Mossbauer_data\Fe-alpha.txt', cor="red", altura=-1.4e7)
+interacao_quadruplar(r'Espectroscopia Mössbauer\Dados\Mossbauer_data\aco_inox.txt',cor="gray",altura=-1.4e4)
 interacao_quadruplar('Espectroscopia Mössbauer\Dados\Mossbauer_data\FeSO4.txt',cor="blue",altura=-5e5)
 interacao_quadruplar('Espectroscopia Mössbauer\Dados\Mossbauer_data\Hematita.txt',cor="green",altura=-1.4e5)
 
 def interacoes_heperfinas(file_name,cor,altura):
+    #calcula os ponto de interação hiperfina
     # Ler dados do arquivo
     dados = pd.read_csv(file_name, sep='\t', usecols=[0, 2], names=["Velocidade da fonte (mm/s)", "Contagem de fontos"])
     x = dados["Velocidade da fonte (mm/s)"]
@@ -96,15 +100,15 @@ def interacoes_heperfinas(file_name,cor,altura):
     # Plot do gráfico
     plt.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
     
-    # Linha de referência em x=0
+    # Linha de referência em x=0 apenas para ajudar na visualização
     # plt.axvline(x=0, color='black', linestyle='-')
     
     # Definir limites de x para encontrar os picos
     x_min = -9
     x_max = 9
-    mask = (x > x_min) & (x < x_max)  # Apenas valores no intervalo -1 < x < 1
+    mask = (x > x_min) & (x < x_max)  
     
-    # Filtrar os dados para o intervalo desejado
+    # Filtrar os dados para o intervalo desejado, o eixo x e y são limitados nesse intervalo
     x_lim = x[mask]
     y_lim = y[mask]
     
